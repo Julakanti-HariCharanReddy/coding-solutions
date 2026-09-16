f4850306a341fb0af473e17aa854f31f6ae9048a
JulakantiHariCharanReddy <hcreddy0007@gmail.com>
2026-09-16 20:50:41 +0530

    ✅ REDBLUE7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/REDBLUE7/solution.java
+++ codechef/medium/REDBLUE7/solution.java
@@ -8,14 +8,32 @@ class Codechef
 	{
 		// your code goes here
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		while(n-->0){
-		    int p = sc.nextInt();
-		     int[] k = new int[p];
-		     for(int i = 0; i< p;i++){
-		         k[i]=sc.nextInt();
-		     }
-		     
+		int j= sc.nextInt();
+		while(j-->0){
+		      int n = sc.nextInt();
+            long[] a = new long[n];
+           long []p = new long[n + 1];
+            long sum = 0;
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextLong();
+                sum += a[i];
+            }
+
+            Arrays.sort(a);
+
+            for (int i = 0; i < n; i++)
+                p[i + 1] = p[i] + a[i];
+
+            long ans = 0;
+
+            for (int r = 1; r < n; r++) {
+                long sr = (r <= n / 2) ? sum - p[n]- r : p[r];            
+                long val = sr * (n - r) + (sum - sr) * r;
+                ans = Math.max(ans, val);
+            }
+
+            System.out.println(ans);
 		}
 
 	}
