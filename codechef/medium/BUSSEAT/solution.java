d04f0d814dabe6acab759f5483e7bae48c8c1fef
JulakantiHariCharanReddy <hcreddy0007@gmail.com>
2026-09-16 20:21:36 +0530

    ✅ BUSSEAT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/BUSSEAT/solution.java
+++ codechef/medium/BUSSEAT/solution.java
@@ -7,6 +7,18 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		while (T--) {
+        int N = sc.nextInt();
+        int K = sc.nextInt();
 
-	}
+        if (K <= N) {
+            System.out.printf("0\n");
+        } else {
+            System.out.printf("%d\n", 2 * (K - N));
+        }
+    }
+
+}
+	
 }
