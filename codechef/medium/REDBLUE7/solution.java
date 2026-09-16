928c01c50a6c331cf6918a8572f15919b79ad178
JulakantiHariCharanReddy <hcreddy0007@gmail.com>
2026-09-16 20:46:28 +0530

    ✅ REDBLUE7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/medium/REDBLUE7/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		while(n-->0){
+		    int p = sc.nextInt();
+		     int[] k = new int[p];
+		     for(int i = 0; i< p;i++){
+		         k[i]=sc.nextInt();
+		     }
+		     
+		}
+
+	}
+}
