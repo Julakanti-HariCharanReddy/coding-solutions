aaf6bc82003e1eaae730f5595ae4b790455423d8
JulakantiHariCharanReddy <hcreddy0007@gmail.com>
2026-09-17 07:20:40 +0530

    ✅ BUSSEAT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-17

--- codechef/medium/BUSSEAT/solution.java
+++ codechef/medium/BUSSEAT/solution.java
@@ -9,7 +9,7 @@ class Codechef
 		// your code goes here
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		while (T--) {
+		while (T-->0) {
         int N = sc.nextInt();
         int K = sc.nextInt();
 
