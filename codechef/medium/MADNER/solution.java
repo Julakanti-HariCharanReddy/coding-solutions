3d142a64f847874d45b703a679f77a80d93e3fb6
JulakantiHariCharanReddy <hcreddy0007@gmail.com>
2026-09-21 22:09:15 +0530

    ✅ MADNER | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/MADNER/solution.java
+++ codechef/medium/MADNER/solution.java
@@ -1,29 +1,18 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+class Solution {
+    public void solve(Scanner sc) {
+        String s = sc.next();
+        int pairs = 0;
+        int i = 0;
+        int n = s.length();
         
-        int t = sc.nextInt();
-        
-        while (t > 0) {
-            String s = sc.next();
-            int pairs = 0;
-            int i = 0;
-            int n = s.length();
-            
-            while (i < n - 1) {
-                if (s.charAt(i) != s.charAt(i + 1)) {
-                    pairs++;
-                    i += 2;
-                } else {
-                    i++;
-                }
+        while (i < n - 1) {
+            if (s.charAt(i) != s.charAt(i + 1)) {
+                pairs++;
+                i += 2;
+            } else {
+                i++;
             }
-            System.out.println(pairs);
-            t--;
         }
-        
-        sc.close();
+        System.out.println(pairs);
     }
 }
