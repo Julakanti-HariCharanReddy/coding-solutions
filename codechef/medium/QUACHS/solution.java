2f96ef44a2679f358039205d75fbad3348a90842
JulakantiHariCharanReddy <hcreddy0007@gmail.com>
2026-09-21 20:22:06 +0530

    ✅ QUACHS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/QUACHS/solution.java
+++ codechef/medium/QUACHS/solution.java
@@ -7,19 +7,6 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		Scanner sc = new Scanner(System.in);
-		int a[] = new int[3];
-	
-		int sum = 0;
-		for (int i = 0;i < 3 ;i++ ){
-		    a[i] = sc.nextInt();
-		} 
-		for (int i = 0;i < 2 ;i++ ){
-		    sum = sum  + a[i];
-		} 
-		int mul = 2*a[2];
-		int sub = sum - mul;
-		System.out.print(sub);
 
 	}
 }
