192754f8865549bd62c5df1c94ed3f492fe09360
JulakantiHariCharanReddy <hcreddy0007@gmail.com>
2026-09-21 22:24:15 +0530

    ✅ MADNER | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/MADNER/solution.java
+++ codechef/medium/MADNER/solution.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 class Solution {
-    public int findMaximumPairs(String s) {
+    public void solve(Scanner sc) {
+        String s = sc.next();
         int pairs = 0;
         int i = 0;
         int n = s.length();
-        
+
         while (i < n - 1) {
             if (s.charAt(i) != s.charAt(i + 1)) {
                 pairs++;
@@ -12,6 +15,21 @@ class Solution {
                 i++;
             }
         }
-        return pairs;
+        System.out.println(pairs);
+    }
+}
+
+ class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int t = sc.nextInt();
+            Solution sol = new Solution();
+            while (t > 0) {
+                sol.solve(sc);
+                t--;
+            }
+        }
+        sc.close();
     }
 }
