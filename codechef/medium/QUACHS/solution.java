332a0474bfa27c7d59cc0e8bef0a39e5d1eb1618
JulakantiHariCharanReddy <hcreddy0007@gmail.com>
2026-09-21 19:35:07 +0530

    ✅ QUACHS | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/QUACHS/solution.java
+++ codechef/medium/QUACHS/solution.java
@@ -8,13 +8,13 @@ class Codechef
 	{
 		// your code goes here
 		Scanner sc = new Scanner(System.in);
-		int a[] = new int[];
-		int n = sc.nextInt();
+		int a[] = new int[3];
+	
 		int sum = 0;
-		for (int i = 0;i < n ;i++ ){
+		for (int i = 0;i < 3 ;i++ ){
 		    a[i] = sc.nextInt();
 		} 
-		for (int i = 0;i < n-1 ;i++ ){
+		for (int i = 0;i < 2 ;i++ ){
 		    sum = sum  + a[i];
 		} 
 		int mul = 2*a[2];
