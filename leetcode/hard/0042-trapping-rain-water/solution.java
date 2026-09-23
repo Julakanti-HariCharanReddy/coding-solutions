02902130753f26a9b0058d83b62130e1ee4c470a
JulakantiHariCharanReddy <hcreddy0007@gmail.com>
2026-09-23 15:57:54 +0530

    ✅ Trapping Rain Water | Hard | Java | Runtime: 0 ms (100.00%) | Memory: 47.7 MB | 2026-09-23

--- leetcode/hard/0042-trapping-rain-water/solution.java
+++ leetcode/hard/0042-trapping-rain-water/solution.java
@@ -1,29 +1,22 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length-1;
-        int lm = height[0];
-        int rm = height[n];
         int l = 0;
         int r = height.length-1;
+         int lm = height[l];
+        int rm = height[r];
         int temp = 0;
 
-        while(l<=r){
-            if (lm > height[l]){
-                temp  = lm - height[l] + temp;
-                //lm= Math.max(lm,height[l]);
-                l++;
-            }else{
-                 lm= Math.max(lm,height[l]);
-                r--;
-            }
-            if(rm > height[r]){
-                temp = rm - height[r] + temp;
-                //rm= Math.max(rm,height[r]);
-                r--;
-            }else{
-                l++;
-                  rm= Math.max(rm,height[r]);
-            }
+        while(l<r){
+           if(lm < rm){
+            l++;
+            lm = Math.max(lm,height[l]);
+            temp += lm - height[l];
+           }
+           else{
+            r--;
+            rm = Math.max(rm, height[r]);
+            temp += rm - height[r];
+             }
         }
         return temp;
 
